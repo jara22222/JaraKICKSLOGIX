89227b0bf986c4acffe3306b8ae4cec05dc3d1a2
jara22222 <jarajoaquin750@gmail.com>
2026-02-23 15:47:52 +0800

    Added QRString  Column in database

---
+++ Server/Migrations/20260223073713_Added_QrString_Product_Table.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_QrString_Product_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QrString",
+                table: "Products",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QrString",
+                table: "Products");
+        }
+    }
+}
